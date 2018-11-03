1069ce03235251652e237e3b5933fe5b1966e622
Kyle J Duffy <kylejduffy@kyles-mbp.dhcp.nd.edu>
2018-11-03 18:39:08 -0400

    with mouse tracking

--- main.js
+++ main.js
@@ -64,7 +64,9 @@ function makeRoom(inRoom) {
 makeRoom(room);
 
 var myObj = { x:100, y:50, name:"desk", id:1 }; 
+var myObj2 = { x:130, y:300, name:"chair", id:2 };
 makeFrame(myObj);
+makeFrame(myObj2);
 
 c.onmousemove = function(e) {
 	var rect = this.getBoundingClientRect(),
@@ -81,3 +83,34 @@ c.onmousemove = function(e) {
 		}
 	}
 }
+
+function objToList(obj) {
+	var list = [];
+	for (var key in obj) {
+		if (typeof obj[key] === 'object') {
+			list.push(objToList(obj[key]));
+		} else {
+			list.push(obj[key]);
+		}
+	}
+	return list;
+}
+
+// borrowed from github.com/substack/point-in-polygon
+function inside(p, vs) {
+	var x = p.x, y = p.y;
+	var inside = false;
+	for (var i = 0, j = vs.length - 1; i < vs.length; j = i++) {
+		var xi = vs[i][0], yi = vs[i][1];
+		var xj = vs[j][0], yj = vs[j][1];
+
+		var intersect = ((yi > y) != yj > y)
+			&& (x < (xj - xi) * (y - yi) / (yj - yi) + xi);
+		if (intersect) inside = !inside;
+	}
+	return inside;
+}
+console.log(inside({x:10,y:10}, objToList(room)));
+console.log(inside({x:999,y:999}, objToList(room)));
+
+
